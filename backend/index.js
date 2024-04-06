cc2768a972a93949d0c75ec1db42b386cad06794
jainam-b <jainambagrecha16@gmail.com>
2024-04-06 10:17:44 +0530

    setup done

--- backend/index.js
+++ backend/index.js
@@ -3,15 +3,20 @@ const app = express();
 const mainRouter =require("./routes/index")
 const userRouter =require("./routes/user")
 const PORT = 3000;
+// cors setup
 const cors = require("cors");
 app.use(cors());
+
 app.use(express.json()); //body parser
-var cors = require('cors')
+
+
+
+// api
 app.use("/api/v1",mainRouter)
 app.use("/api/v1",userRouter)
 
  
- 
+// port 
 app.listen(PORT, function(err){
     if (err) console.log("Error in server setup")
     console.log("Server listening on Port", PORT);
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,10 +1,23 @@
 const express=require("express")
 const router=express.Router();
-
+router.use(express.json());
 router.get("/user",(req,res)=>{
     res.json({
         msg:"in user.js"
     })
+}
+)
+
+
+router.post("/signup",(req,res)=>{
+    const {username,firstname,lastname,password}=req.body
+    console.log({username});
+
+    res.json({
+        meg:"succes"
+    })
+     
+
 })
 
 module.exports=router
