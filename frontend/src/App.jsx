b9836292886a9fd7cbdd96b13c2202cb4e05a684
Jainam Bagrecha <jainambagrecha16@gmail.com>
2024-06-05 23:06:15 +0530

    frotend setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,25 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Signup from "./components/Signup";
+import Signin from "./components/Signin";
+import Dashboard from "./components/Dashboard";
+import SendMoney from "./components/SendMoney";
+
+
 
 function App() {
-
   return (
-    <div>
-        Hello world
-    </div>
-  )
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/send" element={<SendMoney />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
+
 
 export default App
---
+++ frontend/src/components/SendMoney.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SendMoney = () => {
+  return (
+    <div>
+      <h1>asdfasdf</h1>
+    </div>
+  )
+}
+
+export default SendMoney
